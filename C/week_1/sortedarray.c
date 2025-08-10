ce12a17c3f41ba92bcf18f6a6d6f69f4600e7664
Shreya-sinha26 <mail2shreyasinha@gmail.com>
2025-08-10 22:18:32 +0530

    Create sortedarray.c

---
+++ C/week_1/sortedarray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++) 
+    scanf("%d", &arr[i]);
+    for (i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            printf("Not Sorted\n");
+            return 0;
+        }
+    }
+    printf("Sorted\n");
+    return 0;
+}
